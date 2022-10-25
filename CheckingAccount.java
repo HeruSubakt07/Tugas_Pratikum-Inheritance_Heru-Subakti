53c74ccfc30fbc03414acfbc3489db31d9135fa2
HeruSubakt07 <114379114+HeruSubakt07@users.noreply.github.com>
2022-10-25 23:55:00 +0700

    Add files via upload

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,46 +1,32 @@
+package praktikumInheritance;
+
 public class CheckingAccount extends Account {
 
-    // Default Transaction Fee
-    private static double FEE = 2.5;
+    private double overdraftProtection;
 
-    // default constructor
-    public CheckingAccount() {
-        super();
+    public CheckingAccount(double balance, double protect) {
+        super(balance);
+        this.overdraftProtection = protect;
     }
-    
-    public CheckingAccount(int accountNumber, double fee) {
-        super(accountNumber);
-        FEE = fee;
+
+    public CheckingAccount(double balance) {
+        this(balance, -1);
     }
 
     @Override
-    public void deposit(double amount) {
-
-        if( amount > 0) {
-            balance += amount;
-            System.out.printf("Amount %.2f deposited%n", amount);
-            balance -= FEE;
-            System.out.printf("Fee %.2f Applied%n", FEE);
-            System.out.printf("Current Balance is: %.2f%n", balance);
-            
+    public boolean withDraw(double amt) {
+        if (balance - amt >= 0) {
+            balance -= amt;
+            return true;
         } else {
-            System.out.println("A negative amount cannot be deposited");
-        }
-    }
-    @Override
-    public void withdraw(double amount) {
-        if(amount > 0) {
-          
-            if((amount+FEE) <= balance) {
-
-                System.out.printf("Amount of %.2f withdrawn from Account%n", amount);
-                balance -= amount;
-                balance -= FEE;
-                System.out.printf("Fee of %.2f applied%n", FEE);
-                System.out.printf("Current Balance is: %.2f%n", balance);
+            if (overdraftProtection <= amt - balance) {
+                return false;
+            }else{
+                balance=0;
+                overdraftProtection-=amt-balance;
+                return true;
             }
-        } else {
-            System.out.println("Negative amount cannot be withdrawn!");
         }
     }
+
 }
